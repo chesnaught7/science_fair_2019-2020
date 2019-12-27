b27757683f1dcbc55b622d568b6d9a8c6d33b403
Adhitya Thirumala <adhi.thirumala@gmail.com>
2019-12-27 12:58:24 -0500

    even more graphs

--- hundogenderdist.py
+++ hundogenderdist.py
@@ -4,24 +4,23 @@ import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+m = 0
+f = 0
+
 data = pd.read_csv('./hundo.csv')
 df = pd.DataFrame(data)
 print(df)
-m = 0
-f = 0
 df = df[~df["C"].str.contains("No Finding")]
-new_df = df[~df["Patient Gender"].str.contains("M")]
-dr = df[~df["Patient Gender"].str.contains("F")]
-
+new_df = df[df["Patient Gender"].str.contains("M")]
+df_1 = df[df["Patient Gender"].str.contains("F")]
 
 for index, row in new_df.iterrows():
     m += 1
-for index, row in dr.iterrows():
+for index, row in df_1.iterrows():
     f += 1
-
-objects = ("M", "F")
+objects = ("Male", "Female")
 y_pos = np.arange(len(objects))
-performance = [m , f]
+performance = [m, f]
 
 font = {'family' : 'normal','weight' : 'bold','size'   : 8}
 fig, ax = plt.subplots()
@@ -31,9 +30,9 @@ ax.set_yticks(y_pos)
 ax.set_yticklabels(objects)
 ax.invert_yaxis()  # labels read top-to-bottom
 ax.set_xlabel('Gender')
-ax.set_title("Gender Distribution")
+ax.set_title("Gender Distribution for Cardiomegaly")
 ax.tick_params(axis='both', which='major', labelsize=5)
 ax.tick_params(axis='both', which='minor', labelsize=5)
 
 plt.show()
-plt.savefig('hundogenderdistnf.pdf')
+plt.savefig('genderdistnf.pdf')
