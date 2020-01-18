d135a047e529461797dec20eb84658813e477059
Adhitya Thirumala <adhi.thirumala@gmail.com>
2020-01-18 14:56:27 -0500

    sorter is complete

--- sorter.py
+++ sorter.py
@@ -6,28 +6,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
-data = pd.read_csv('cardiomegaly (copy).csv')
+data = pd.read_csv('cardiomegaly.csv')
 df = pd.DataFrame(data)
 print(df)
 
 ap = 0
 pa = 0
 for index, row in df.iterrows():
-    if 'Cardiomegaly' in row['C']
-        ap += 1
-    elif row['View Position'] == 'PA':
-        pa += 1
-print(ap)
-print(pa)
-
-
-objects = ('AP', 'PA')
-y_pos = np.arange(len(objects))
-performance = [ap, pa]
-
-plt.bar(y_pos, performance, align='center', alpha=0.5)
-plt.xticks(y_pos, objects)
-plt.ylabel('View Position')
-plt.title('View Positions of all X-rays')
-
-plt.savefig('demo.pdf')
+    if 'Cardiomegaly' in row['C']:
+        print(row[0])
+        os.rename('/media/adhit/Iomega/images/' + str(row[1]), '/media/adhit/Iomega/images/cardiomegaly/' + str(row[1]))
+    elif 'No Finding' in row['C']:
+        print(row[0])
+        os.rename("/media/adhit/Iomega/images/" + str(row[1]), '/media/adhit/Iomega/images/nf' + str(row[1]))
